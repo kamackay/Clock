c8abec9208d902e8f89f4e8c91d29f867996d4f5
Keith MacKay <kmackay@corp.ipipenet.com>
2016-05-24 08:45:02 -0400

    started showing analog back

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+const dataName = 'clocktype';
+var clockType = '';
+
+function getType() {
+    return getData(dataName);
+}
+
+function setType(str) {
+    storeData(dataName, str);
+}
+
+$(window).resize(setSizes);
+
 $(document).ready(function () {
     $('#contents').css('height', $(window).height());
     setSizes();
-
+    clockType = getType();
+    if (clockType == 'analog') {
+        $('#analogTab').addClass('active');
+        showAnalog();
+    } else if (clockType == 'digital') {
+        $('#digitalTab').addClass('active');
+        showDigital();
+    } else {
+        $('#bothTab').addClass('active');
+        showBoth();
+    }
 });
 
 function setSizes() {
@@ -9,4 +32,30 @@ function setSizes() {
     $.each(types, function (x, o) {
         $(this).css('width', $(window).width() / types.length);
     });
+}
+
+function tabClick(e) {
+    setType(e.target.innerHTML.toLowerCase());
+}
+
+function clearClock() {
+    $('#clock-contents').html('');
+}
+
+function showAnalog() {
+    try {
+        $('#clock-contents').append('<div id="keithapps-clock" style="position: fixed; padding: 50px;">' +
+            '<img style="position: absolute; height: 100%; " src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-circle.png" />' + '</div>');
+        var clock = $('#keithapps-clock');
+        clock.css('height', $(window).height() - 250).css('width', $(window).width());
+    } catch (err) {}
+}
+
+function showDigital() {
+    try {} catch (err) {}
+
+}
+
+function showBoth() {
+    try {} catch (err) {}
 }
