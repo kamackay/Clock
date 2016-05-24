e3364e71df9090e9b9777ff1300843f173b1bc3b
Keith MacKay <kmackay@corp.ipipenet.com>
2016-05-24 13:08:46 -0400

    now shows unanimated analog

--- script.js
+++ script.js
@@ -44,10 +44,24 @@ function clearClock() {
 
 function showAnalog() {
     try {
-        $('#clock-contents').append('<div id="keithapps-clock" style="position: fixed; padding: 50px;">' +
-            '<img style="position: absolute; height: 100%; " src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-circle.png" />' + '</div>');
-        var clock = $('#keithapps-clock');
-        clock.css('height', $(window).height() - 250).css('width', $(window).width());
+        $('#clock-contents').append('<div id="keithapps-clock" style="position: fixed;">' +
+            '<img id="keithapps-clockImg" style="position: absolute; height: 100%;" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-circle2.png" />' + '</div>');
+        var clockEl = $("#keithapps-clock");
+        var size = $(window).height() * .85;
+        clockEl.css('width', size);
+        clockEl.css('height', size);
+        clockEl.css('top', 75);
+        clockEl.css('left', ($(window).width() - size) / 2);
+        clockEl.append('<div id="keithapps-secondParent" style="height: 100%; width: 100%; position: absolute;">' + '<img id="keithapps-clockSecond" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-hand2.png" style="position: absolute; height: 50%; top: 5px;" />' + '</div>');
+        clockEl.append('<div id="keithapps-minuteParent" style="height: 100%; width: 100%; position: absolute; ">' + '<img id="keithapps-clockMinute" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-hand2.png" style="position: absolute; height: 50%; padding-top: 10%; top: 5px; z-index: 0;" />' + '</div>');
+        clockEl.append('<div id="keithapps-hourParent" style="height: 100%; width: 100%; position: absolute; city: .5;">' + '<img id="keithapps-clockHour" src="https://googledrive.com/host/0B6vDuBGkfv-HSjhIcnJEUF9yc0k/clock-handRed2.png" style="position: absolute; height: 50%; padding-top: 18%; top: 5px; opacity: .5; z-index: 0;" />' + '</div>');
+        var secondHand = $('#keithapps-clockSecond');
+        var minuteHand = $('#keithapps-clockMinute');
+        var hourHand = $('#keithapps-clockHour');
+        var l = (clockEl.width() - secondHand.width()) / 2;
+        secondHand.css('left', l);
+        minuteHand.css('left', l);
+        hourHand.css('left', l);
     } catch (err) {}
 }
 
